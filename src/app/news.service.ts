acbebfbe2bff375741b02ecc6c28ab1c743b0be4
rcasironman <karthikmangineni0603@gmail.com>
2025-02-10 03:50:08 +0530

    Resolved cors issue for external api call

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -7,7 +7,9 @@ import { map, catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class NewsService {
-  private apiUrl = 'https://newsapi.org/v2/top-headlines';
+  // private apiUrl = 'https://newsapi.org/v2/top-headlines';
+  private apiUrl = 'https://script.google.com/macros/s/AKfycbxySg8sTbVuDwr_sOc3orIuSFDxH4VQbrvHTPCoBmfDalzv1S5c7V6w0qrmUmcqXNzXFg/exec';
+
 
   constructor(private http: HttpClient) {}
 
